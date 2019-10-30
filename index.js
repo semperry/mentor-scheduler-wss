3d8a2daa0646f6a4e6eb885dec7dcaec7a12bf66
semperry <ryan.curtis88@gmail.com>
2019-10-30 11:05:02 -0600

    cleaned up log statements

--- index.js
+++ index.js
@@ -1,15 +1,15 @@
-// TODO: cleanup log statements
 const WebSocket = require("ws");
 const express = require("express");
-const PORT = process.env.PORT || 8080;
+const port = process.env.PORT || 8080;
 
 const app = express();
 
 const server = new WebSocket.Server({
-  server: app.listen(PORT, () => console.log(`listening on ${PORT}`))
+  server: app.listen(port, () => console.log(`listening on ${port}`))
 });
 
 server.on("connection", socket => {
+  console.log(`${socket} has joined`);
   socket.on("message", message => {
     server.clients.forEach(client => {
       console.log(JSON.parse(message));
